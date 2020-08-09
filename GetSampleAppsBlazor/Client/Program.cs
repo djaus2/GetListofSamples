3d46d8b46401b45314fb5fb48857a63d2794db80
djaus2 <davidjones@sportronics.com.au>
2020-08-10 05:01:10 +1000

    Renamed Blazor project and folder. Added sample project file

---
+++ GetSampleAppsBlazor/Client/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+
+
+
+namespace GetSampleApps.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient<Services.SamplesClient>(client =>
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+            await builder.Build().RunAsync();
+
+
+        }
+    }
+}
